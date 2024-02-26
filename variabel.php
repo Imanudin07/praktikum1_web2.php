cd0a5d2cd2a2f58563feb21960bdb6dc8e9e9d50
Imanudin07 <146207121+Imanudin07@users.noreply.github.com>
2024-02-26 03:06:37 -0800

    Add files via upload
    php praktikum 1

---
+++ variabel.php
@@ -0,0 +1,10 @@
+<?php
+
+$nama = "Imanudin";
+$jurusan = "Sistem Informasi";
+$semester = 2;
+
+
+echo 'nama saya : ' .$nama;
+echo '</br> jurusan : ' .$jurusan;
+echo '</br> semester : ' .$semester;
